1ed3927cae4e81df74e0e1a4211dd9a3e474910e
TIVMOF <78173711+TIVMOF@users.noreply.github.com>
2024-10-01 16:58:04 +0300

    Moving data project to main project (#6)

---
+++ codbex-appointments/gen/codbex-appointments/ui/Appointment/Appointment/controller.js
@@ -0,0 +1,295 @@
+angular.module('page', ["ideUI", "ideView", "entityApi"])
+	.config(["messageHubProvider", function (messageHubProvider) {
+		messageHubProvider.eventIdPrefix = 'codbex-appointments.Appointment.Appointment';
+	}])
+	.config(["entityApiProvider", function (entityApiProvider) {
+		entityApiProvider.baseUrl = "/services/ts/codbex-appointments/gen/codbex-appointments/api/Appointment/AppointmentService.ts";
+	}])
+	.controller('PageController', ['$scope', '$http', 'messageHub', 'entityApi', 'Extensions', function ($scope, $http, messageHub, entityApi, Extensions) {
+
+		$scope.dataPage = 1;
+		$scope.dataCount = 0;
+		$scope.dataOffset = 0;
+		$scope.dataLimit = 10;
+		$scope.action = "select";
+
+		//-----------------Custom Actions-------------------//
+		Extensions.get('dialogWindow', 'codbex-appointments-custom-action').then(function (response) {
+			$scope.pageActions = response.filter(e => e.perspective === "Appointment" && e.view === "Appointment" && (e.type === "page" || e.type === undefined));
+		});
+
+		$scope.triggerPageAction = function (action) {
+			messageHub.showDialogWindow(
+				action.id,
+				{},
+				null,
+				true,
+				action
+			);
+		};
+		//-----------------Custom Actions-------------------//
+
+		function refreshData() {
+			$scope.dataReset = true;
+			$scope.dataPage--;
+		}
+
+		function resetPagination() {
+			$scope.dataReset = true;
+			$scope.dataPage = 1;
+			$scope.dataCount = 0;
+			$scope.dataLimit = 10;
+		}
+
+		//-----------------Events-------------------//
+		messageHub.onDidReceiveMessage("clearDetails", function (msg) {
+			$scope.$apply(function () {
+				$scope.selectedEntity = null;
+				$scope.action = "select";
+			});
+		});
+
+		messageHub.onDidReceiveMessage("entityCreated", function (msg) {
+			refreshData();
+			$scope.loadPage($scope.dataPage, $scope.filter);
+		});
+
+		messageHub.onDidReceiveMessage("entityUpdated", function (msg) {
+			refreshData();
+			$scope.loadPage($scope.dataPage, $scope.filter);
+		});
+
+		messageHub.onDidReceiveMessage("entitySearch", function (msg) {
+			resetPagination();
+			$scope.filter = msg.data.filter;
+			$scope.filterEntity = msg.data.entity;
+			$scope.loadPage($scope.dataPage, $scope.filter);
+		});
+		//-----------------Events-------------------//
+
+		$scope.loadPage = function (pageNumber, filter) {
+			if (!filter && $scope.filter) {
+				filter = $scope.filter;
+			}
+			if (!filter) {
+				filter = {};
+			}
+			$scope.selectedEntity = null;
+			entityApi.count(filter).then(function (response) {
+				if (response.status != 200) {
+					messageHub.showAlertError("Appointment", `Unable to count Appointment: '${response.message}'`);
+					return;
+				}
+				if (response.data) {
+					$scope.dataCount = response.data;
+				}
+				$scope.dataPages = Math.ceil($scope.dataCount / $scope.dataLimit);
+				filter.$offset = ($scope.dataPage - 1) * $scope.dataLimit;
+				filter.$limit = $scope.dataLimit;
+				if ($scope.dataReset) {
+					filter.$offset = 0;
+					filter.$limit = $scope.dataPage * $scope.dataLimit;
+				}
+
+				entityApi.search(filter).then(function (response) {
+					if (response.status != 200) {
+						messageHub.showAlertError("Appointment", `Unable to list/filter Appointment: '${response.message}'`);
+						return;
+					}
+					if ($scope.data == null || $scope.dataReset) {
+						$scope.data = [];
+						$scope.dataReset = false;
+					}
+
+					response.data.forEach(e => {
+						if (e.IntakeDate) {
+							e.IntakeDate = new Date(e.IntakeDate);
+						}
+						if (e.ReleaseDate) {
+							e.ReleaseDate = new Date(e.ReleaseDate);
+						}
+					});
+
+					$scope.data = $scope.data.concat(response.data);
+					$scope.dataPage++;
+				});
+			});
+		};
+		$scope.loadPage($scope.dataPage, $scope.filter);
+
+		$scope.selectEntity = function (entity) {
+			$scope.selectedEntity = entity;
+			messageHub.postMessage("entitySelected", {
+				entity: entity,
+				selectedMainEntityId: entity.Id,
+				optionsOperator: $scope.optionsOperator,
+				optionsAssigee: $scope.optionsAssigee,
+				optionsStatus: $scope.optionsStatus,
+				optionsSalesOrder: $scope.optionsSalesOrder,
+				optionsCustomer: $scope.optionsCustomer,
+			});
+		};
+
+		$scope.createEntity = function () {
+			$scope.selectedEntity = null;
+			$scope.action = "create";
+
+			messageHub.postMessage("createEntity", {
+				entity: {},
+				optionsOperator: $scope.optionsOperator,
+				optionsAssigee: $scope.optionsAssigee,
+				optionsStatus: $scope.optionsStatus,
+				optionsSalesOrder: $scope.optionsSalesOrder,
+				optionsCustomer: $scope.optionsCustomer,
+			});
+		};
+
+		$scope.updateEntity = function () {
+			$scope.action = "update";
+			messageHub.postMessage("updateEntity", {
+				entity: $scope.selectedEntity,
+				optionsOperator: $scope.optionsOperator,
+				optionsAssigee: $scope.optionsAssigee,
+				optionsStatus: $scope.optionsStatus,
+				optionsSalesOrder: $scope.optionsSalesOrder,
+				optionsCustomer: $scope.optionsCustomer,
+			});
+		};
+
+		$scope.deleteEntity = function () {
+			let id = $scope.selectedEntity.Id;
+			messageHub.showDialogAsync(
+				'Delete Appointment?',
+				`Are you sure you want to delete Appointment? This action cannot be undone.`,
+				[{
+					id: "delete-btn-yes",
+					type: "emphasized",
+					label: "Yes",
+				},
+				{
+					id: "delete-btn-no",
+					type: "normal",
+					label: "No",
+				}],
+			).then(function (msg) {
+				if (msg.data === "delete-btn-yes") {
+					entityApi.delete(id).then(function (response) {
+						if (response.status != 204) {
+							messageHub.showAlertError("Appointment", `Unable to delete Appointment: '${response.message}'`);
+							return;
+						}
+						refreshData();
+						$scope.loadPage($scope.dataPage, $scope.filter);
+						messageHub.postMessage("clearDetails");
+					});
+				}
+			});
+		};
+
+		$scope.openFilter = function (entity) {
+			messageHub.showDialogWindow("Appointment-filter", {
+				entity: $scope.filterEntity,
+				optionsOperator: $scope.optionsOperator,
+				optionsAssigee: $scope.optionsAssigee,
+				optionsStatus: $scope.optionsStatus,
+				optionsSalesOrder: $scope.optionsSalesOrder,
+				optionsCustomer: $scope.optionsCustomer,
+			});
+		};
+
+		//----------------Dropdowns-----------------//
+		$scope.optionsOperator = [];
+		$scope.optionsAssigee = [];
+		$scope.optionsStatus = [];
+		$scope.optionsSalesOrder = [];
+		$scope.optionsCustomer = [];
+
+
+		$http.get("/services/ts/codbex-employees/gen/codbex-employees/api/Employees/EmployeeService.ts").then(function (response) {
+			$scope.optionsOperator = response.data.map(e => {
+				return {
+					value: e.Id,
+					text: e.Name
+				}
+			});
+		});
+
+		$http.get("/services/ts/codbex-employees/gen/codbex-employees/api/Employees/EmployeeService.ts").then(function (response) {
+			$scope.optionsAssigee = response.data.map(e => {
+				return {
+					value: e.Id,
+					text: e.Name
+				}
+			});
+		});
+
+		$http.get("/services/ts/codbex-appointments/gen/codbex-appointments/api/Settings/AppointmentStatusService.ts").then(function (response) {
+			$scope.optionsStatus = response.data.map(e => {
+				return {
+					value: e.Id,
+					text: e.Name
+				}
+			});
+		});
+
+		$http.get("/services/ts/codbex-orders/gen/codbex-orders/api/SalesOrder/SalesOrderService.ts").then(function (response) {
+			$scope.optionsSalesOrder = response.data.map(e => {
+				return {
+					value: e.Id,
+					text: e.Name
+				}
+			});
+		});
+
+		$http.get("/services/ts/codbex-partners/gen/codbex-partners/api/Customers/CustomerService.ts").then(function (response) {
+			$scope.optionsCustomer = response.data.map(e => {
+				return {
+					value: e.Id,
+					text: e.Name
+				}
+			});
+		});
+
+		$scope.optionsOperatorValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsOperator.length; i++) {
+				if ($scope.optionsOperator[i].value === optionKey) {
+					return $scope.optionsOperator[i].text;
+				}
+			}
+			return null;
+		};
+		$scope.optionsAssigeeValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsAssigee.length; i++) {
+				if ($scope.optionsAssigee[i].value === optionKey) {
+					return $scope.optionsAssigee[i].text;
+				}
+			}
+			return null;
+		};
+		$scope.optionsStatusValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsStatus.length; i++) {
+				if ($scope.optionsStatus[i].value === optionKey) {
+					return $scope.optionsStatus[i].text;
+				}
+			}
+			return null;
+		};
+		$scope.optionsSalesOrderValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsSalesOrder.length; i++) {
+				if ($scope.optionsSalesOrder[i].value === optionKey) {
+					return $scope.optionsSalesOrder[i].text;
+				}
+			}
+			return null;
+		};
+		$scope.optionsCustomerValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsCustomer.length; i++) {
+				if ($scope.optionsCustomer[i].value === optionKey) {
+					return $scope.optionsCustomer[i].text;
+				}
+			}
+			return null;
+		};
+		//----------------Dropdowns-----------------//
+
+	}]);
